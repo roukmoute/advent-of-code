c6ec58c29063f48f75c990a6f7131d633339899a
Mathias STRASSER <contact@roukmoute.fr>
2023-12-06 00:19:01 +0100

    Day1 - Part Two

--- 2023/spec/Day1/Day1Spec.php
+++ 2023/spec/Day1/Day1Spec.php
@@ -18,6 +18,13 @@ class Day1Spec extends ObjectBehavior
         $this->findFirstDigit('pqr3stu8vwx')->shouldReturn(3);
         $this->findFirstDigit('a1b2c3d4e5f')->shouldReturn(1);
         $this->findFirstDigit('treb7uchet')->shouldReturn(7);
+        $this->findFirstDigit('two1nine')->shouldReturn(2);
+        $this->findFirstDigit('eightwothree')->shouldReturn(8);
+        $this->findFirstDigit('abcone2threexyz')->shouldReturn(1);
+        $this->findFirstDigit('xtwone3four')->shouldReturn(2);
+        $this->findFirstDigit('4nineeightseven2')->shouldReturn(4);
+        $this->findFirstDigit('zoneight234')->shouldReturn(1);
+        $this->findFirstDigit('7pqrstsixteen')->shouldReturn(7);
     }
 
     function it_should_found_last_digit()
@@ -26,6 +33,13 @@ class Day1Spec extends ObjectBehavior
         $this->findLastDigit('pqr3stu8vwx')->shouldReturn(8);
         $this->findLastDigit('a1b2c3d4e5f')->shouldReturn(5);
         $this->findLastDigit('treb7uchet')->shouldReturn(7);
+        $this->findLastDigit('two1nine')->shouldReturn(9);
+        $this->findLastDigit('eightwothree')->shouldReturn(3);
+        $this->findLastDigit('abcone2threexyz')->shouldReturn(3);
+        $this->findLastDigit('xtwone3four')->shouldReturn(4);
+        $this->findLastDigit('4nineeightseven2')->shouldReturn(2);
+        $this->findLastDigit('zoneight234')->shouldReturn(4);
+        $this->findLastDigit('7pqrstsixteen')->shouldReturn(6);
     }
 
     function it_should_form_a_single_two_digit_number()
@@ -34,6 +48,13 @@ class Day1Spec extends ObjectBehavior
         $this->formSingleTwoDigitNumber('pqr3stu8vwx')->shouldReturn(38);
         $this->formSingleTwoDigitNumber('a1b2c3d4e5f')->shouldReturn(15);
         $this->formSingleTwoDigitNumber('treb7uchet')->shouldReturn(77);
+        $this->formSingleTwoDigitNumber('two1nine')->shouldReturn(29);
+        $this->formSingleTwoDigitNumber('eightwothree')->shouldReturn(83);
+        $this->formSingleTwoDigitNumber('abcone2threexyz')->shouldReturn(13);
+        $this->formSingleTwoDigitNumber('xtwone3four')->shouldReturn(24);
+        $this->formSingleTwoDigitNumber('4nineeightseven2')->shouldReturn(42);
+        $this->formSingleTwoDigitNumber('zoneight234')->shouldReturn(14);
+        $this->formSingleTwoDigitNumber('7pqrstsixteen')->shouldReturn(76);
     }
 
     function it_add_multiple_lines_together()
@@ -41,12 +62,13 @@ class Day1Spec extends ObjectBehavior
         $this->addMultipleLinesTogether('1abc2', 'pqr3stu8vwx')->shouldReturn(50);
         $this->addMultipleLinesTogether('a1b2c3d4e5f', 'treb7uchet')->shouldReturn(92);
         $this->addMultipleLinesTogether('1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet')->shouldReturn(142);
+        $this->addMultipleLinesTogether('two1nine', 'eightwothree', 'abcone2threexyz', 'xtwone3four', '4nineeightseven2', 'zoneight234', '7pqrstsixteen')->shouldReturn(281);
     }
 
     function it_should_sum_of_all_of_the_calibration_values()
     {
         $file = file_get_contents(dirname(__DIR__, 2) . '/src/Day1/input/day1.txt');
         $lines = explode("\n", $file);
-        $this->addMultipleLinesTogether(...$lines)->shouldReturn(54573);
+        $this->addMultipleLinesTogether(...$lines)->shouldReturn(54591);
     }
 }
--- 2023/src/Day1/Day1.php
+++ 2023/src/Day1/Day1.php
@@ -6,24 +6,45 @@ class Day1
 {
     public function findFirstDigit(string $value): int
     {
-        for ($i = 0; $i < strlen($value); $i++) {
+        $letters = '';
+        $i = -1;
+        $maxLen = strlen($value);
+        if ($maxLen === 0) {
+            return 0;
+        }
+        while (++$i < $maxLen) {
             if (is_numeric($value[$i])) {
                 return (int) $value[$i];
             }
-        }
+            $letters .= $value[$i];
 
-        return 0;
+            if (preg_match('/one|two|three|four|five|six|seven|eight|nine/', $letters)) {
+                $letters = str_replace(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'], [1, 2, 3, 4, 5, 6, 7, 8, 9], $letters);
+
+                return (int) filter_var($letters, FILTER_SANITIZE_NUMBER_INT);
+            }
+        }
     }
 
     public function findLastDigit(string $value): int
     {
-        for ($i = strlen($value) - 1; $i >= 0; $i--) {
+        $letters = '';
+        $i = strlen($value);
+        if ($i === 0) {
+            return 0;
+        }
+        while (--$i >= 0) {
             if (is_numeric($value[$i])) {
                 return (int) $value[$i];
             }
-        }
+            $letters = $value[$i].$letters;
 
-        return 0;
+            if (preg_match('/one|two|three|four|five|six|seven|eight|nine/', $letters)) {
+                $letters = str_replace(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'], [1, 2, 3, 4, 5, 6, 7, 8, 9], $letters);
+
+                return (int) filter_var($letters, FILTER_SANITIZE_NUMBER_INT);
+            }
+        }
     }
 
     public function formSingleTwoDigitNumber($argument1)
